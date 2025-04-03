f9cdfd528564d40225f0b0012d8bb12dcd3dd9b8
Jeff Levin <jeff@levinology.com>
2025-04-03 15:20:43 -0800

    working symlinks

--- Makefile
+++ Makefile
@@ -8,26 +8,9 @@ bootstrap:
 	# Start the daemon
 	sudo launchctl kickstart -k system/org.nixos.nix-daemon
 	cd ~/.config/nixpkgs && nix run . -- switch --flake .
-	#echo "nix installed. open a new shell in this dir and run \"make finish-setup\""
-
-#finish-setup:
-	#cd ~/.config/nixpkgs && nix run .
-#  # Install nix-darwin using the Nix package manager
-#  nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
-#  nix-channel --update
-#  # Rebuild and switch
-#  ~/.nix-profile/bin/darwin-rebuild switch --flake ~/.config/nixpkgs
-#  ~/.nix-profile/bin/home-manager switch -b backup
 
 update:
-	cd ~/.config/nixpkgs && \
-		~/.nix-profile/bin/darwin-rebuild switch --flake . && \
-		~/.nix-profile/bin/home-manager switch -b backup
-
-# A simpler target that just updates the system with the current config
-quick-update:
-	~/.nix-profile/bin/darwin-rebuild switch --flake ~/.config/nixpkgs
-	~/.nix-profile/bin/home-manager switch -b backup
+	nix run . -- switch --flake .
 
 nuke:
 	@./scripts/nuke-nix.sh
--- home.nix
+++ home.nix
@@ -1,27 +1,25 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   home.username = "jeff";
   home.homeDirectory = "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/jeff";
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
 
+
   home.file = {
-    # demo vim config
-    ".vimrc".source = config.lib.file.mkOutOfStoreSymlink ./vim_configuration
-
-    # rc files
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/zshrc"
-    ".sqliterc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/sqliterc"
-    ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gitconfig"
-    ".gitignore".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gitignore"
-    ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/tmux.conf"
-    ".gemrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gemrc"
-
-    # directories
-    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/nvim"
-    ".config/1Password".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/config/1Password";
-    ".config/op".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/config/op";
+# demo vim config
+    ".vimrc".source = config.lib.file.mkOutOfStoreSymlink ./vim_configuration;
+# rc files
+    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/zshrc";
+    ".sqliterc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/sqliterc";
+    ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gitconfig";
+    ".gitignore".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gitignore";
+    ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/tmux.conf";
+    ".gemrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/gemrc";
+# directories
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/nvim";
+    ".config/1Password".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/1Password";
+    ".config/op".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/configs/op";
   };
-
 
   #imports = [ ./packages.nix ./vim.nix ./git.nix ./rest.nix ];
 }
