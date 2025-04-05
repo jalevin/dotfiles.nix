7615419d4e6fcf319ffae9aa5c9c8871f7c55b8a
Jeff Levin <jeff@levinology.com>
2025-04-04 20:19:51 -0800

    checkpoint

--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
 {
   description = "Home Manager configuration";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+
+    # not sure what this does
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nix-index-database, ... }:
+
+  outputs = { self, nixpkgs, nixpkgs-stable, nix-darwin, home-manager, nix-index-database, ... }:
     let
       withArch = arch:
         home-manager.lib.homeManagerConfiguration {
@@ -29,20 +48,19 @@
     in {
       # Define the apps to provide the home-manager command
       apps = {
-        x86_64-darwin.default = {
-          type = "app";
-          program = "${home-manager.packages.x86_64-darwin.home-manager}/bin/home-manager";
-        };
         aarch64-darwin.default = {
           type = "app";
           program = "${home-manager.packages.aarch64-darwin.home-manager}/bin/home-manager";
         };
-        aarch64-linux.default = {
-          type = "app";
-          program = "${home-manager.packages.aarch64-linux.home-manager}/bin/home-manager";
-        };
+        #x86_64-darwin.default = {
+        #  type = "app";
+        #  program = "${home-manager.packages.x86_64-darwin.home-manager}/bin/home-manager";
+        #};
+        #aarch64-linux.default = {
+        #  type = "app";
+        #  program = "${home-manager.packages.aarch64-linux.home-manager}/bin/home-manager";
       };
-      
+
       # Define a convenience app for switching
       apps.aarch64-darwin.switch = {
         type = "app";
