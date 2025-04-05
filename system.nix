b0ebd2641d0366515a65a62478e42695ec964f5b
Jeff Levin <jeff@levinology.com>
2025-04-05 14:40:59 -0800

    cleanup

--- system.nix
+++ system.nix
@@ -3,25 +3,7 @@
   # use the id from determinate installer
   ids.gids.nixbld = 350;
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    neovim
-    ripgrep
-    curl
-    wget
-  ];
-
-  homebrew = {
-    enable = true;
-    global.autoUpdate = false;
-
-    #casks = [ "kitty" ];
-  };
-
   users.users.${user}.home = "/Users/${user}";
-
-  #nix.enable = false;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
@@ -35,4 +17,55 @@
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = architecture;
+
+  imports = [ ./osx-settings.nix ];
+
+  ## PACKAGES
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  #environment.systemPackages = with pkgs; [
+  #  neovim
+  #  ripgrep
+  #  curl
+  #  wget
+  #];
+  environment.systemPackages = [
+    pkgs.ansible
+    pkgs.bat
+    pkgs.coreutils
+    pkgs.curl
+    pkgs.gitFull
+    pkgs.jsonnet
+    pkgs.jsonnet-bundler
+    pkgs.neovim
+    pkgs.nmap
+    pkgs.htop
+    pkgs.tldr
+    pkgs.jq
+    pkgs.nodejs
+    pkgs.ripgrep
+    pkgs.tree
+    pkgs.watch
+    pkgs.wget
+  ]
+
+  ## BREW
+  homebrew = {
+    enable = true;
+    global.autoUpdate = false;
+
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap"; # Uninstalls all formulae not listed here
+    };
+
+    brews = [
+      # CLI tools via Homebrew
+    ];
+
+    casks = [
+      # GUI apps via Homebrew
+      "whatsapp"
+    ];
+  };
 }
